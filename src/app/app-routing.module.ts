60f42a610e3cf530eebc1f085dde5e2f0db3ca0a
Jim Fulto <jimfulto233@gmail.com>
2021-11-09 10:44:24 +0530

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormListComponent } from './form-list/form-list.component';
+import { HashirasComponent } from './hashiras/hashiras.component';
 
-const routes: Routes = [];
+const approutes: Routes = [
+  { path: '', redirectTo: '/hashiras', pathMatch: 'full' },
+  { path: 'hashiras', component: HashirasComponent },
+  { path: 'form-list', component: FormListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(approutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
